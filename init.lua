bf76d48be82425701b80076edc231d1366fd1933
t1ld3 <71977639+t1ld3@users.noreply.github.com>
2022-01-12 18:40:14 +0100

    fix formatting

--- init.lua
+++ init.lua
@@ -24,11 +24,11 @@ require("user.notify")
 local status_ok, notify = pcall(require, "notify")
 if status_ok then
 	vim.notify = notify
-for _, v in ipairs(files) do
-	---@diagnostic disable-next-line: redefined-local
-	local status_ok, _ = pcall(require, "user." .. v)
-	if not status_ok then
-		vim.notify(v .. ".lua not found!", vim.log.levels.ERROR)
+	for _, v in ipairs(files) do
+		---@diagnostic disable-next-line: redefined-local
+		local status_ok, _ = pcall(require, "user." .. v)
+		if not status_ok then
+			vim.notify(v .. ".lua not found!", vim.log.levels.ERROR)
+		end
 	end
-end
 end
