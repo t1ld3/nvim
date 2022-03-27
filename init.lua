780f0d6088f60880f10e9a7ebb5d0f9bfec3d5c4
t1ld3 <71977639+t1ld3@users.noreply.github.com>
2022-03-27 12:33:03 +0200

    fix colorscheme + formatting

--- init.lua
+++ init.lua
@@ -18,17 +18,18 @@ local files = {
 	"alpha",
 	"which-key",
 	"toggleterm",
+  "neoscroll",
 }
 vim.opt.termguicolors = true
 require("user.notify")
 local status_ok, notify = pcall(require, "notify")
 if status_ok then
 	vim.notify = notify
-	for _, v in ipairs(files) do
-		---@diagnostic disable-next-line: redefined-local
-		local status_ok, _ = pcall(require, "user." .. v)
-		if not status_ok then
-			vim.notify(v .. ".lua not found!", vim.log.levels.ERROR)
-		end
+end
+for _, v in ipairs(files) do
+	---@diagnostic disable-next-line: redefined-local
+	local status_ok, _ = pcall(require, "user." .. v)
+	if not status_ok then
+		vim.notify(v .. ".lua not found!", vim.log.levels.ERROR)
 	end
 end
