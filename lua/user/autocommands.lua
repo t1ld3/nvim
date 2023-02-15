3e362fb76e764d968591289b1a9c3d79266ac141
T1ld3 <till-nordine.perrier@epitech.eu>
2023-02-15 15:46:53 +0100

    feat: put default autocommands in an augroup

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -1,16 +1,19 @@
-vim.api.nvim_create_autocmd({ "VimResized" }, { callback = function() vim.cmd "tabdo wincmd =" end })
+local augroup = vim.api.nvim_create_augroup("default_group", { clear = true })
+vim.api.nvim_create_autocmd({ "VimResized" }, { group = augroup, callback = function() vim.cmd "tabdo wincmd =" end })
 -- Use 'q' to quit from common plugins
 vim.api.nvim_create_autocmd({ "FileType" }, {
+  group = augroup,
   pattern = { "qf", "help", "man", "lspinfo", "spectre_panel", "lir" },
   callback = function()
     vim.cmd [[
-      nnoremap <silent> <buffer> q :close<CR> 
-      set nobuflisted 
+      nnoremap <silent> <buffer> q :close<CR>
+      set nobuflisted
     ]]
   end,
 })
 -- Remove statusline and tabline when in Alpha
 vim.api.nvim_create_autocmd({ "User" }, {
+  group = augroup,
   pattern = { "AlphaReady" },
   callback = function()
     vim.cmd [[
@@ -21,22 +24,34 @@ vim.api.nvim_create_autocmd({ "User" }, {
 })
 -- Set wrap and spell in markdown and gitcommit
 vim.api.nvim_create_autocmd({ "FileType" }, {
+  group = augroup,
   pattern = { "gitcommit", "markdown" },
   callback = function()
     vim.opt_local.wrap = true
     vim.opt_local.spell = true
   end,
 })
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  group = augroup,
+  pattern = { "make" },
+  callback = function()
+    vim.opt_local.tabstop = 8
+    vim.opt_local.shiftwidth = 0
+  end
+})
 vim.api.nvim_create_autocmd({ "BufEnter" },
-  { callback = function() vim.cmd "if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif" end })
+  { group = augroup,
+    callback = function() vim.cmd "if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif" end })
 -- Fixes Autocomment
 vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
+  group = augroup,
   callback = function()
     vim.cmd "set formatoptions-=cro"
   end,
 })
 -- Highlight Yanked Text
 vim.api.nvim_create_autocmd({ "TextYankPost" }, {
+  group = augroup,
   callback = function()
     vim.highlight.on_yank { higroup = "Visual", timeout = 200 }
   end,
